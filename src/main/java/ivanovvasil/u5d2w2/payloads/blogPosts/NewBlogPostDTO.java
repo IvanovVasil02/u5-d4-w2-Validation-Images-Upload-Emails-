74e367e3836e1bfb05133ecc82aa21af4a3e80ce
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-11-09 15:14:34 +0100

    creation of records fot new user and new blog post

---
+++ src/main/java/ivanovvasil/u5d2w2/payloads/blogPosts/NewBlogPostDTO.java
@@ -0,0 +1,18 @@
+package ivanovvasil.u5d2w2.payloads.blogPosts;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record NewBlogPostDTO(
+        @NotEmpty(message = "The category is a required field")
+        @Size(min = 4, max = 30, message = "The name must be between 3 and 30 characters")
+        String categoria,
+        @NotEmpty(message = "The title is a required field")
+        @Size(min = 4, max = 30, message = "The surname must be between 3 and 30 characters")
+        String titolo,
+        @NotEmpty(message = "The post content is a required field")
+        @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "The email entered is invalid")
+        String postContent
+) {
+}
